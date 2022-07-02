d1aa9e33c1f83070f4eb1bcc746c20b0885de233
SouradeepBera <souradeepb1000@gmail.com>
2022-07-02 18:54:27 +0530

    using stream in try with resources, removing unused methods

--- src/main/java/com/sprinklr/javasip/utils/AudioHelper.java
+++ src/main/java/com/sprinklr/javasip/utils/AudioHelper.java
@@ -5,8 +5,6 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.*;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
 import java.nio.file.Files;
 
 /*
@@ -30,73 +28,8 @@ public class AudioHelper {
         if (!isWavFile) {
             throw new IllegalArgumentException("Unsupported encoding " + outputFile);
         }
-        AudioInputStream audioStream = getAudioStream(data);
-        AudioSystem.write(audioStream, AudioFileFormat.Type.WAVE, Files.newOutputStream(outputFile.toPath()));
-    }
-
-    /*
-     * Returns the audio stream corresponding to the array of bytes
-     *
-     * @param byteArray the byte array
-     * @return the converted audio stream
-     */
-    public static AudioInputStream getAudioStream(byte[] byteArray) throws IOException, UnsupportedAudioFileException {
-
-        try {
-            ByteArrayInputStream byteStream =
-                    new ByteArrayInputStream(byteArray);
-            return AudioSystem.getAudioInputStream(byteStream);
-        } catch (UnsupportedAudioFileException e) {
-            byteArray = addWavHeader(byteArray);
-            ByteArrayInputStream byteStream =
-                    new ByteArrayInputStream(byteArray);
-            return AudioSystem.getAudioInputStream(byteStream);
+        try(AudioInputStream audioStream = AudioSystem.getAudioInputStream(new ByteArrayInputStream(data))) {
+            AudioSystem.write(audioStream, AudioFileFormat.Type.WAVE, Files.newOutputStream(outputFile.toPath()));
         }
-    }
-
-    /*
-     * Adds a WAV header to the byte array
-     *
-     * @param bytes the original array of bytes
-     * @return the new array with the header
-     * @throws IOException if the byte array is ill-formatted
-     */
-    private static byte[] addWavHeader(byte[] bytes) {
-
-        ByteBuffer bufferWithHeader = ByteBuffer.allocate(bytes.length + 44);
-        bufferWithHeader.order(ByteOrder.LITTLE_ENDIAN);
-        bufferWithHeader.put("RIFF".getBytes());
-        bufferWithHeader.putInt(bytes.length + 36);
-        bufferWithHeader.put("WAVE".getBytes());
-        bufferWithHeader.put("fmt ".getBytes());
-        bufferWithHeader.putInt(16);
-        bufferWithHeader.putShort((short) 1);
-        bufferWithHeader.putShort((short) 1);
-        bufferWithHeader.putInt(16000);
-        bufferWithHeader.putInt(32000);
-        bufferWithHeader.putShort((short) 2);
-        bufferWithHeader.putShort((short) 16);
-        bufferWithHeader.put("data".getBytes());
-        bufferWithHeader.putInt(bytes.length);
-        bufferWithHeader.put(bytes);
-        return bufferWithHeader.array();
-    }
-
-    /*
-     * Reads the input stream and returns the corresponding array of bytes
-     *
-     * @param stream the initial stream
-     * @return the array of bytes from the stream
-     */
-    public static byte[] readStream(InputStream stream) throws IOException {
-        ByteArrayOutputStream rawBuffer = new ByteArrayOutputStream();
-        int nRead;
-        byte[] data = new byte[1024 * 16];
-        while ((nRead = stream.read(data, 0, data.length)) != -1) {
-            rawBuffer.write(data, 0, nRead);
-        }
-        rawBuffer.flush();
-        rawBuffer.close();
-        return rawBuffer.toByteArray();
     }
 }
